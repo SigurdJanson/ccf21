3fc29f92565d75ad82b7bbf795b704e23e6a4a43
SigurdJanson <emailjs@web.de>
2020-03-19 19:18:49 +0100

    Added shifting function including unit tests

---
+++ R/ccf21.R
@@ -0,0 +1,101 @@
+
+
+.Cor_ccf <- function(x, y, n = NA, mean = NA, sttdev = NA) {
+  # if mean and stddev are given, treat x and y as stationary time series
+  # if not, compute your own mean and stddev
+}
+
+
+.HandleLost_ccf <- function() {
+}
+
+
+
+.Shift_ccf <- function(y, k, replace = FALSE) {
+  # Different ways to handle the sequence parts that protrude: 
+  # replace = FALSE: cut it (today's default), 
+  # replace = TRUE:  wrap it back assuming the sequence is circular, 
+  # replace = as.double(anyhting): or fill it with data.
+  
+  # PRECONDITIONS
+  if(k == 0) return(y)
+  
+  ly <- length(y)
+  if(abs(k) >= ly) stop("|k| is greater or equal than length of y.
+                        There are no values left.")
+  if (!is.na(replace) && !isTRUE(replace) && !isFALSE(replace) ) 
+    if (!is.numeric(replace)) stop("'replace' must be numeric")
+
+  # OPERATIONS
+  y2 <- y[seq(max(-k+1, 1), min(ly, ly-k))]
+  if (!isFALSE(replace)) {
+    if (isTRUE(replace)) {
+      if(k > 0)
+        rp <- y[seq(ly-k+1, ly)]
+      else
+        rp <- y[1:(-k)]
+    } else {
+      rp <- rep(replace, abs(k))
+    }
+    if (k > 0)
+      y2 <- c(rp, y2)
+    else
+      y2 <- c(y2, rp)
+  }
+
+  # FINISH
+  return(y2)
+}
+
+
+#'
+#'
+#'
+#' 
+ccf <- function (x, y, lag.max = NULL, type = c("correlation", "covariance"), 
+                 stationary = TRUE, 
+                 protrusionaction = NULL, 
+                 windowaction = c("cut", "imprison"),
+                 plot = TRUE, na.action = na.fail, ...)  {
+  # PRECONDITIONS
+  if (is.matrix(x) || is.matrix(y)) 
+    stop("univariate time series only.")
+  if ( (is.ts(x) || is.ts(y)) && !stationary )
+    stop("Conflicting arguments x/y and 'stationary': time series objects shall be assumed to be stationarity.")
+  
+  LenX <- length(x)
+  LenY <- length(y)
+  if (LenX != LenY && missing(windowaction)) 
+    stop("'windowaction' has been set despite equal length of 'x' and 'y'.")
+  
+  # PREPARE
+  if (missing(windowaction))
+    windowaction <- "cut"
+  else
+    windowaction <- match.arg(windowaction)
+  
+  # Classic ccf call
+  if(stationary && missing(protrusionaction) && windowaction == "cut") {
+    X <- ts.intersect(as.ts(x), as.ts(y))
+    colnames(X) <- c(deparse(substitute(x))[1L], deparse(substitute(y))[1L])
+    acf.out <- stats::acf(X, lag.max = lag.max, plot = FALSE, type = type, 
+                   na.action = na.action)
+    lag <- c(rev(acf.out$lag[-1, 2, 1]), acf.out$lag[, 1, 2])
+    y <- c(rev(acf.out$acf[-1, 2, 1]), acf.out$acf[, 1, 2])
+    acf.out$acf <- array(y, dim = c(length(y), 1L, 1L))
+    acf.out$lag <- array(lag, dim = c(length(y), 1L, 1L))
+    acf.out$snames <- paste(acf.out$snames, collapse = " & ")
+  }
+  
+  
+  
+  # Finish up
+  # acf.out <- structure(list(acf = acf, type = type, n.used = sampleT, 
+  #                           lag = lag, series = series, snames = colnames(x)), class = "acf")
+  if (plot) {
+    plot.acf(acf.out, ...)
+    invisible(acf.out)
+  }
+  else 
+    return(acf.out)
+}
---
+++ test/shift_test.R
@@ -0,0 +1,49 @@
+library(testthat)
+source("../R/ccf21.R")
+
+test_that(".Shift_ccf", {
+  # Errors
+  expect_error(.Shift_ccf(1:2, 1, replace = "FALSE"), 
+               "'replace' must be numeric")
+  expect_error(.Shift_ccf(1, -1, replace = 0), 
+               "When |k| is greater or equal than the length of y")
+  expect_error(.Shift_ccf(1, 1, replace = 0), 
+               "When |k| is greater or equal than the length of y")
+  
+  # Cycle-Shift forward
+  s <- 1:20
+  for ( k in 0:(length(s)-1) ) {
+    o <- .Shift_ccf(s, k, replace = TRUE)
+    e <- c(seq(20-k+1, 20, length.out = k), seq(1, 20-k))
+    expect_equal(o, e)
+  }
+  
+  # Cycle-Shift backward
+  s <- 1:20
+  for ( k in 0:(-length(s)+1) ) {
+    o <- .Shift_ccf(s, k, replace = TRUE)
+    e <- c(seq(-k+1, 20), seq(1, -k, length.out = -k))
+    expect_equal(o, e)
+  }
+  
+  # Replace-Shift forward
+  for(r in c(0, 99, NA)) {
+    s <- 1:20
+    for ( k in 0:(length(s)-1) ) {
+      o <- .Shift_ccf(s, k, replace = r)
+      e <- c(rep(r, k), seq(1, 20-k))
+      expect_equal(o, e)
+    }
+  }
+  
+  # Replace-Shift backward
+  for(r in c(0, 99, NA)) {
+    s <- 1:20
+    for ( k in 0:(-length(s)+1) ) {
+      o <- .Shift_ccf(s, k, replace = r)
+      e <- c(seq(-k+1, 20), rep(r, -k))
+      expect_equal(o, e)
+    }
+  }
+  
+})
