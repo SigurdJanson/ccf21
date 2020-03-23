481b200eb07f410c2e77ce37c1517e587c9fe30c
SigurdJanson <emailjs@web.de>
2020-03-23 21:30:39 +0100

    SpLit 'summary' and 'print.summary'

--- R/summaryccf21.R
+++ R/summaryccf21.R
@@ -1,41 +1,101 @@
 
 #' summary.ccf
 #' Summary for a cross-correlation object `ccf`.
-#' @param object an object of class `ccf`.
-#' @param digits integer, used for number formatting with `signif()`.
+#' @param obj an object of class `ccf`.
+#' @param digits the number of significant digits to use when printing.
 #'
-#' @return This method returns an object of class `c("table")`.
+#' @return This function computes and returns a list of summary statistics:
+#' \describe{
+#'   \item{type}{Used statistics correlation or covariance.}
+#'   \item{smethod}{The shifting method used in call to `ccf`.}
+#'   \item{nacount}{The number of NAs in the statistics.}
+#'   \item{range}{The number of observations in sequence.}
+#'   \item{min}{Minimum correlation/covariance found.}
+#'   \item{minpos}{Index positions where the maxima were found.}
+#'   \item{max}{Maximum correlation/covariance found.}
+#'   \item{maxpos}{Index positions where the maxima were found.}
+#' }
+#' 
 #' @export
+#' @author Jan Seifert
 #' @examples summary(ccf(1:10, 10:1))
-summary.ccf <- function(object, digits) {
-  if (is.null(object)) return(NULL)
+summary.ccf <- function(obj, digits) {
+  if (is.null(obj)) return(NULL)
   if (missing(digits)) digits = 4L
   
-  type <- match(object$type, c("correlation", "covariance"))
+  # Correlations or Covariance
+  sumry <- list()
+  sumry$type    <- obj$type   
+  sumry$smethod <- list(obj$shiftaction, 
+                        ifelse(obj$shiftaction == "replace", 
+                               obj$replacedby, 
+                               NA_real_)) 
+  # Count NAs
+  sumry$nacount <- sum(is.na(obj$acf)) 
+  # Range of correlations
+  sumry$range  <- range(obj$acf)
+  # Maximum/minimum correlation at which lags?
+  absval <- abs(obj$acf)
+  sumry$min    <- min(absval)
+  sumry$minpos <- obj$lag[ which.min(absval) ]
+  sumry$max    <- max(absval)
+  sumry$maxpos <- obj$lag[ which.max(absval) ]
+
+  class(sumry) <- c("summaryccf")
+  return(sumry)
+}
+
+
+
+#' print.summaryccf
+#'
+#' @param x an object of class "`summaryccf`", usually, a result of 
+#' a call to `summary.ccf`.
+#' @param digits the number of significant digits to use when printing.
+#'
+#' @export
+#'
+#' @author Jan Seifert
+#' @examples
+print.summaryccf <- function (x, digits = max(3, getOption("digits") - 3L)) {
+  type <- match(x$type, c("correlation", "covariance"))
   abbrev <- c("Cor", "Cov")
   
-  # List of information
-  info <- c("= Definition =", "Type", "Shift method", 
-            "",
-            "= Values =", "NAs", "Range", paste("Maximum", abbrev[type]))
-  # Table object as template
+  # = Definition section =
+  # 1. Compile it
+  info <- c("Type", "Shift method")
   sumry <- array("", c(length(info), 1L), list(info, c("Details")))
-  # Correlations or Covariance
-  sumry[2L, 1L] <- object$type   
-  if (object$shiftaction == "replace") # Method Replace with ...
-    sumry[3L, 1L] <- paste0("replace with \"", object$replacedby, "\"") 
+  sumry[1L, 1L] <- type
+  if (x$smethod[[1]] == "replace") # Method Replace with ...
+    sumry[2L, 1L] <- paste0("replace with \"", x$smethod[[2]], "\"") 
   else # Method: Wrap, Cut, Imprison
-    sumry[3L, 1L] <- object$shiftaction 
-  # Has NAs? Count them.
-  sumry[6L, 1L] <- sum(is.na(object$acf)) 
-  # Range of correlations
-  sumry[7L, 1L] <- paste(signif(range(object$acf), digits), 
-                         collapse = " to ")
-  # Maximum absolute correlation at which lags?
-  sumry[8L, 1L] <- paste0(signif(max(abs(object$acf)), digits), 
-                        " at position(s) ", 
-                        toString( which.max(abs(object$acf))) )
+    sumry[2L, 1L] <- x$smethod[[1]]
+  #class(sumry) <- c("table")
   
-  class(sumry) <- c("summaryccf", "table")
-  sumry
+  # 2. Print it
+  cat("Definition:\n\n")
+  print.table(sumry)
+  
+  # = Content section =
+  # 1. Compile it
+  info <- c("NAs", "Range", paste("Minimum", abbrev[type]), 
+            paste("Maximum", abbrev[type]))
+  sumry <- array("", c(length(info), 1L), list(info, c("Details")))
+  sumry[1L, 1L] <- x$nacount
+  sumry[2L, 1L] <- paste(signif(x$range, digits), collapse = " to ")
+  sumry[3L, 1L] <- paste0( signif(x$min, digits), 
+                           " at lag ", 
+                           toString(signif(x$minpos, digits)) )
+  sumry[4L, 1L] <- paste0( signif(x$max, digits), 
+                           " at lag ", 
+                           toString(signif(x$maxpos, digits)) )
+  
+  # 2. Print it
+  cat("\n\nValues:\n\n")
+  print.table(sumry)
 }
+
+
+
+y <- summary(o)
+print(y)
