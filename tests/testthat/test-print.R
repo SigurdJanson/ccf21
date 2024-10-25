91aeaf812380efc3c7150cf2f9192e591871e71d
Jan <59577271+SigurdJanson@users.noreply.github.com>
2024-10-25 18:16:07 +0200

    Update test-print.R
    Tests corrected and improved

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -8,29 +8,26 @@ test_that("print.ccf21", {
   # note: for some unknown reason print.default prints only
   # as much decimals as needed. It ignores 'digits'. It
   # does the final printing.
-  result <- ccf( 1:10, 1:10, shiftaction = "cut", lag.max = 5,
-                 plot = FALSE )
+  result <- ccf( 1:10, 1:10, shiftaction = "cut", lag.max = 5, plot = FALSE )
   expect_output(print(result), "Cross-Correlation")
   expect_output(print(result), "lag cor")
-  expect_output(print(result), "-5  1")
-  expect_output(print(result), "0  1")
-  expect_output(print(result), "5  1")
+  for (lag in -5:5)
+    expect_output(print(result), paste0(lag, "\\s*", 1))
 
   result <- ccf( 1:10, 1:10, shiftaction = "cut", lag.max = 5,
                  type = "covariance", plot = FALSE )
   expect_output(print(result), "Cross-Covariance")
   expect_output(print(result), "lag cov")
   #
-  expect_output(print(result, digits = 2), "-5  2.5")
-  expect_output(print(result, digits = 2),  "0  9.2")
-  expect_output(print(result, digits = 2),  "5  2.5")
+  e <- c(2.0, 2.9166666, 4.0, 5.25, 6.666666, 8.25, 6.666666, 5.25, 4.0, 2.9166666, 2.0)
   # default for 'digits' is (7-3)
-  expect_output(print(result), "-5  2.500")
-  expect_output(print(result),  "0  9.167")
-  expect_output(print(result),  "5  2.500")
-  #
-  expect_output(print(result, digits = 6), "-5  2.50000")
-  expect_output(print(result, digits = 6),  "0  9.16667")
-  expect_output(print(result, digits = 6),  "5  2.50000")
+  for (lag in -5:5)
+    expect_output(print(result), paste0(lag, "\\s*", format(e[lag+6L], digits=4)))
+
+  for (lag in -5:5)
+    expect_output(print(result, digits = 2), paste0(lag, "\\s*", format(e[lag+6L], digits=2)))
+
+  for (lag in -5:5)
+    expect_output(print(result, digits=6), paste0(lag, "\\s*", format(e[lag+6L], digits=6L)))
 })
 
